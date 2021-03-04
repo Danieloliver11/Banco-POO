d7e295395ef3c0415a857e40d967d4c6e65b659f
Daniel <69875036+Danieloliver11@users.noreply.github.com>
2021-03-04 17:23:45 -0300

    commit daniel para main

--- Banco-Gear/src/CONTA/Conta.java
+++ Banco-Gear/src/CONTA/Conta.java
@@ -1,73 +1,80 @@
 package CONTA;
 
-
 public class Conta {
-	
+
+	private int id;
 	private int numero;
 	private String cpf;
 	private double saldo;
 	private boolean ativa;
-	
-	public Conta(String cpf) {
-		super(); //?
+
+	 
+    public Conta() {
+    	
+    }
+    
+	public Conta(int id, int numero, String cpf, double saldo, boolean ativa) {
+		super();
+		this.id = id;
 		this.numero = numero;
 		this.cpf = cpf;
-	}
-	
-	//sobrecarga
-	public Conta(int numero,String cpf,boolean ativa) {
-		this.numero = numero;
-		this.cpf = cpf;
+		this.saldo = saldo;
 		this.ativa = ativa;
-		}
+	}
 
-
-
-
-	/////////////
-	//emcapsulamento/ proteção de dados 
-	//mostra
 	public int getNumero() {
 		return numero;
-	}//altera
+	}
+
 	public void setNumero(int numero) {
 		this.numero = numero;
 	}
+
 	public String getCpf() {
 		return cpf;
 	}
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
+
 	public double getSaldo() {
 		return saldo;
 	}
-	/*public void setSaldo(double saldo) {
-		this.saldo = saldo;
-	}*/
+
+	/*
+	 * public void setSaldo(double saldo) { this.saldo = saldo; }
+	 */
 	public boolean isAtiva() {
 		return ativa;
 	}
+
 	public void setAtiva(boolean ativa) {
-		
 		this.ativa = ativa;
 	}
-		///////////////
-	
-	
-	
-	
-	
-	//metodo +
+
 	public void credito(double valor) {
 		this.saldo = this.saldo + valor;
 	}
-	// -
-	public void debito(double valor) {
-		if(valor >=this.saldo) {
-		this.saldo = this.saldo - valor;
-		}else {System.out.println("Você não tem saldo suficiente!");
-			}
-	}
-}
 
+	public void debito(double valor) {
+		if (valor >= this.saldo) {
+			this.saldo = this.saldo - valor;
+		} else {
+			System.out.println("Você não tem saldo suficiente!");
+		}
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+}
--- Banco-Gear/src/CONTA/ContaCorrente.java
+++ Banco-Gear/src/CONTA/ContaCorrente.java
@@ -2,18 +2,48 @@ package CONTA;
 
 public class ContaCorrente extends Conta {
 	private int contadorTalao = 3;
-	
-	public ContaCorrente(int contadorTalao,String cpf) {
-		super(cpf);
-		this.contadorTalao = contadorTalao;
+	private int movimento = 10;
+
+	public ContaCorrente() {
+
 	}
-	
-	
-	public int getContadorTalao (){
+
+	public ContaCorrente(int contadorTalao, int movimento) {
+		this.contadorTalao = contadorTalao;
+		this.movimento = movimento;
+	}
+
+	public int getContadorTalao() {
 		return contadorTalao;
-	}	
-	
-	void setContadorTalao() {
-		this.contadorTalao = contadorTalao;
 	}
+
+	void setContadorTalao() {
+		if (contadorTalao <= 0) {
+			System.out.println("Desculpe você não tem mais talão para ser usado!");
+		} else {
+			this.contadorTalao = this.contadorTalao - 1;
+			System.out.println("Você solicitou um talão.\nVocê tem " + contadorTalao + " Talões restantes");
+		}
+	}
+
+	// movimentos
+	public int getMovimento() {
+		return movimento;
+	}
+
+	void setMovimento() {
+		this.movimento = movimento - 1;
+
+		if (movimento <= 0) {
+			System.out.println("Suas movimentações diarias acabaram! ");
+
+		}
+	}
+	/*
+	 * Solicitar ao final dos 10 movimentos ou ao pedido de sair se o mesmo deseja
+	 * solicitar cheque, limitando em apenas 3 cheques.
+	 * 
+	 * public int movimentos() { int movimento = 10; return movimento; }
+	 */
+
 }
